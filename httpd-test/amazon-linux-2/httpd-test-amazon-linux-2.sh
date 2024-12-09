383c8f137e400dd0f430460880540af08f864968
Gáll Botond <gallbotondofc@gmail.com>
2024-12-09 19:32:36 +0200

    put index location and html content into vars to make it modular

--- httpd-test/amazon-linux-2/httpd-test-amazon-linux-2.sh
+++ httpd-test/amazon-linux-2/httpd-test-amazon-linux-2.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # Use this for your user data (script from top to bottom)
 # install httpd (Linux 2 version)
+
+INDEX_HTML_LOCATION="/var/www/html/index.html"
+HTML_CONTENT="<h1> $(hostname -f)</h1>"
+
 {
   sudo yum update -y
   sudo yum install -y httpd
   sudo systemctl start httpd
   sudo systemctl enable httpd
-  echo "<h1> $(hostname -f)</h1>" | sudo tee /var/www/html/index.html
+  echo "$HTML_CONTENT" | sudo tee $INDEX_HTML_LOCATION
 } >> /var/log/ec2-user-data.log 2>&1
